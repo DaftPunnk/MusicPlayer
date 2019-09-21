73cb7ab162954fe09dd137caaebc4e475ef5c624
ZhaokunChen <chenzhaokun@vip.qq.com>
2019-09-21 11:39:57 +0800

    Initial Commit

---
+++ MusicPlayer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MusicPlayer
+//
+//  Created by Will Chen on 21/09/19.
+//  Copyright © 2019 Will Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
