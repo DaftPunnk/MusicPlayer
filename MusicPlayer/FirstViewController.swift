d8bafd5ae251353c8a3eaa108c09fe38416d4302
ZhaokunChen <chenzhaokun@vip.qq.com>
2019-09-21 14:43:28 +0800

    Getting Started

--- MusicPlayer/FirstViewController.swift
+++ MusicPlayer/FirstViewController.swift
@@ -13,6 +13,35 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        gettingSongName()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func gettingSongName() {
+        let folderURL = URL(fileURLWithPath: Bundle.main.resourcePath!)
+        
+        do {
+            let songPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            
+            for song in songPath {
+                var mySong = song.absoluteString
+                
+                if mySong.contains(".mp3") {
+                    let findString = mySong.components(separatedBy: "/")
+                    mySong =  findString[findString.count-1]
+                    mySong = mySong.replacingOccurrences(of: "%20", with: " ")
+                    mySong = mySong.replacingOccurrences(of: ".mp3", with: "")
+                    print (mySong)
+                }
+            }
+        }
+        catch {
+            
+        }
     }
 
 
